44fad3a7a9a0bdcf7997389caeb9ad7fc678cf2a
Alex <aironer@yandex.ru>
2024-08-20 16:00:35 +0300

    получение имени

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./styles.css";
-import { Book, BookInformation } from "./lib/types";
-import { getBooks, getUsers, getReviews } from "./lib/api";
-import { useEffect, useState, FC } from "react";
-import Card from "./Card";
+import "./styles.css"
+import { Book, BookInformation, User } from "./lib/types"
+import { getBooks, getUsers, getReviews } from "./lib/api"
+import { useEffect, useState, FC } from "react"
+import Card from "./Card"
 
 // Техническое задание:
 // Доработать приложение App, чтобы в отрисованном списке
@@ -19,42 +19,49 @@ import Card from "./Card";
 
 // // В объектах реализующих интерфейс BookInformation, ReviewInformation// указана полная информация об пользователе и обзоре.
 
-const toBookInformation = (book: Book): BookInformation => {
-  // const reviews: Review[] = await getReviews(book.id);
-  // const users: User[] = await getUsers();
+const toBookInformation = (book: Book, users: User[]): BookInformation => {
+  const authorName = users.find((el) => el.id === book.authorId)?.name || "avtor neizvesten"
   return {
     id: book.id,
     name: book.name || "Книга без названия",
-    author: { name: "Test Author", id: "id" },
-    reviews: [
-      { id: "test", text: "test text", user: { id: "sdf", name: "Reviewer" } },
-    ],
+    // array1.find((element) => element > 10);
+    author: { name: authorName, id: book.authorId },
+    reviews: [{ id: "test", text: "test text", user: { id: "sdf", name: "Reviewer" } }],
     description: book.description,
-  };
-};
+  }
+}
 
 const App: FC = () => {
-  const [books, setBooks] = useState<Book[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
+  const [books, setBooks] = useState<Book[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [users, setUsers] = useState<User[]>([])
   useEffect(() => {
     const fetchBooks = async () => {
-      setIsLoading(true);
-      const fetchedBooks = await getBooks();
-      setBooks(fetchedBooks);
-      setIsLoading(false);
-    };
-    fetchBooks();
-  }, []);
+      setIsLoading(true)
+      const fetchedBooks = await getBooks()
+      setBooks(fetchedBooks)
+      setIsLoading(false)
+    }
+    fetchBooks()
+  }, [])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true)
+      const resp = await getUsers()
+      setUsers(resp)
+      setIsLoading(false)
+    }
+    fetchUsers()
+  }, [])
 
   return (
     <div>
       <h1>Мои книги:</h1>
       {isLoading && <div>Загрузка...</div>}
-      {!isLoading &&
-        books.map((b) => <Card key={b.id} book={toBookInformation(b)} />)}
+      {!isLoading && books.map((b) => <Card key={b.id} book={toBookInformation(b, users)} />)}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,12 @@
-import { Book, Review, User } from "./types";
-import { books, reviews, users } from "./mocks";
+import { Book, Review, User } from "./types"
+import { books, reviews, users } from "./mocks"
 
 const getAsync = <T>(value: T[]): Promise<T[]> => {
   return new Promise((res) => {
-    setTimeout(() => res(value), Math.random() * 3000);
-  });
-};
+    setTimeout(() => res(value), Math.random() * 3000)
+  })
+}
 
-export const getBooks = (): Promise<Book[]> => getAsync(books);
-export const getUsers = (): Promise<User[]> => getAsync(users);
-export const getReviews = (): Promise<Review[]> => getAsync(reviews);
+export const getBooks = (): Promise<Book[]> => getAsync(books)
+export const getUsers = (): Promise<User[]> => getAsync(users)
+export const getReviews = (id: number): Promise<Review[]> => getAsync(reviews)
